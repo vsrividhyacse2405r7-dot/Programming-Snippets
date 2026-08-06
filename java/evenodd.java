aa58da61f95f8c708ac2208fa3bab8b2d7f282b9
vsrividhyacse2405r7-dot <vsrividhya_cse2405r7@mgit.ac.in>
2026-08-06 11:10:03 +0530

    Create evenodd.java

---
+++ java/evenodd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if(num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
